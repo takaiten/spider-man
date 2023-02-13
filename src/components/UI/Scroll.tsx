7602ed1ff5ae7769be8f27909791628f3c08b469
takaiten <lopilopj@gmail.com>
2023-02-13 20:00:37 +0400

    Add logo and menu icons

---
+++ src/components/UI/Scroll.tsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect, memo } from 'react';
+import { useScene } from 'babylonjs-hook';
+import styles from './Scroll.module.css';
+
+type ScrollProps = {
+  start?: number;
+  speed?: number;
+};
+
+const Scroll = memo<ScrollProps>(({ start = 1, speed = 0.1 }) => {
+  const [frame, setFrame] = useState(start);
+  const [lastFrame, setLastFrame] = useState(start);
+  const scene = useScene();
+
+  useEffect(() => {
+    const animGroup = scene?.getAnimationGroupByName('cameraAnimations');
+    if (!animGroup) return;
+    setLastFrame(animGroup.to);
+    const handleScroll = (event: WheelEvent) => {
+      setFrame((prev) => {
+        const value = Math.max(start, Math.min(prev + event.deltaY * speed, animGroup.to));
+        animGroup.goToFrame(value);
+        animGroup.play();
+        animGroup.pause();
+        return value;
+      });
+    };
+
+    window.addEventListener('wheel', handleScroll, { capture: false });
+    () => window.removeEventListener('wheel', handleScroll);
+  }, [scene, setFrame]);
+
+  return (
+    <div className={styles.scrollbar}>
+      <div className={styles.scrollbarLine} />
+      <div className={styles.scrollbarCircle} style={{ top: `${(frame / lastFrame) * 100}%` }} />
+    </div>
+  );
+});
+Scroll.displayName = 'Scroll';
+
+export default Scroll;
