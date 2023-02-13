74cc3f19388d078fc1934f37f47f0f3d4c50f486
takaiten <lopilopj@gmail.com>
2023-02-13 17:04:26 +0400

    Add Shin Go font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,29 @@
+import localFont from '@next/font/local';
 import './globals.css';
+
+const shinGo = localFont({
+  src: [
+    {
+      path: '../../public/assets/fonts/G-OTF Shin Go Pro/G-OTF Shin Go Pro L.otf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../../public/assets/fonts/G-OTF Shin Go Pro/G-OTF Shin Go Pro R.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/assets/fonts/G-OTF Shin Go Pro/G-OTF Shin Go Pro B.otf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang="en" className={shinGo.className}>
       <head />
       <body>{children}</body>
     </html>
