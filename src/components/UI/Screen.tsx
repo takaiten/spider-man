d822342eb059f5d8f467171072f2f1d33dc47048
takaiten <lopilopj@gmail.com>
2023-02-13 20:00:37 +0400

    Add logo and menu icons

--- src/components/UI/Screen.tsx
+++ src/components/UI/Screen.tsx
@@ -1,55 +1,30 @@
 'use client';
 
-import React, { memo, useEffect, useState } from 'react';
-import { useScene } from 'babylonjs-hook';
+import React from 'react';
+import Image from 'next/image';
+
 import styles from './Screen.module.css';
+import Scroll from './Scroll';
 
 type ScreenProps = {};
-
-type ScrollProps = {
-  start?: number;
-  speed?: number;
-};
-
-const Scroll = memo<ScrollProps>(({ start = 1, speed = 0.1 }) => {
-  const [frame, setFrame] = useState(start);
-  const [lastFrame, setLastFrame] = useState(start);
-  const scene = useScene();
-
-  useEffect(() => {
-    const animGroup = scene?.getAnimationGroupByName('cameraAnimations');
-    if (!animGroup) return;
-    setLastFrame(animGroup.to);
-    const handleScroll = (event: WheelEvent) => {
-      setFrame((prev) => {
-        const value = Math.max(start, Math.min(prev + event.deltaY * speed, animGroup.to));
-        animGroup.goToFrame(value);
-        animGroup.play();
-        animGroup.pause();
-        return value;
-      });
-    };
-
-    window.addEventListener('wheel', handleScroll, { capture: false });
-    () => window.removeEventListener('wheel', handleScroll);
-  }, [scene, setFrame]);
-
-  return (
-    <div className={styles.scrollbar}>
-      <div className={styles.scrollbarLine} />
-      <div className={styles.scrollbarCircle} style={{ top: `${(frame / lastFrame) * 100}%` }} />
-    </div>
-  );
-});
-Scroll.displayName = 'Scroll';
 
 const Screen: React.FC<ScreenProps> = () => {
   return (
     <div className={styles.container}>
       <header className={styles.header}>
-        <div>
-          <span>🕸</span>&nbsp;
-          <span>menu</span>
+        <div className={styles.headerLeft}>
+          <span className={styles.icon}>
+            <Image
+              loading="eager"
+              src="/assets/vector/hc-icon.svg"
+              width={32}
+              height={32}
+              alt="🕷"
+            />
+          </span>
+          <span>
+            <Image loading="eager" src="/assets/vector/menu.svg" width={32} height={32} alt="+" />
+          </span>
         </div>
         <div>
           <button className={styles.button}>Buy tickets</button>
