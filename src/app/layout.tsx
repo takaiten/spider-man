92fd76c46475cb12cc69786217f5030e857628e8
takaiten <lopilopj@gmail.com>
2023-02-19 00:00:25 +0400

    Add fonts and text blocks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,9 @@
 import './globals.css';
-// TODO: investigate further
-// import localFont from '@next/font/local';
-// const shinGo = localFont({
-//   src: [
-//     {
-//       path: '../../public/assets/fonts/G-OTF Shin Go Pro/G-OTF Shin Go Pro L.otf',
-//       weight: '300',
-//       style: 'normal',
-//     },
-//     {
-//       path: '../../public/assets/fonts/G-OTF Shin Go Pro/G-OTF Shin Go Pro R.otf',
-//       weight: '400',
-//       style: 'normal',
-//     },
-//     {
-//       path: '../../public/assets/fonts/G-OTF Shin Go Pro/G-OTF Shin Go Pro B.otf',
-//       weight: '700',
-//       style: 'normal',
-//     },
-//   ],
-// });
+import { shinGo } from '~/helpers/font';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={shinGo.className}>
       <head />
       <body>{children}</body>
     </html>
