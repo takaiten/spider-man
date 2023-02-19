0bc0199b05ccd09eb274218d3aaf48fdbf4c1347
takaiten <lopilopj@gmail.com>
2023-02-19 12:26:37 +0400

    Implement parallax background

--- src/components/UI/Background.tsx
+++ src/components/UI/Background.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useRef } from 'react';
 import style from './Background.module.css';
 
 const Background: React.FC = () => {
-  return <div className={style.background} />;
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (ref.current) {
+        const percent =
+          -ref.current.getBoundingClientRect().y / (ref.current.offsetHeight - window.innerHeight);
+        ref.current.style.setProperty('--parallax-y', `${(percent * 100) / 2}%`);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <div ref={ref} className={style.bgContainer}>
+      <div className={style.bgImage} />
+    </div>
+  );
 };
 
 export default Background;
